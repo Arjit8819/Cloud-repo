89d9642768d0b497961859c84abb60f8be2257b7
Arjit Kiledar <kiledar.a@husky.neu.edu>
2019-04-11 13:08:44 -0400

    final

--- webapp/NoteApp_Production/Controllers/ValuesController.cs
+++ webapp/NoteApp_Production/Controllers/ValuesController.cs
@@ -118,7 +118,7 @@ namespace trial.Controllers
         }
 
         [HttpPost]
-        [Route("/user/registerA")]
+        [Route("/user/registerArjit")]
         public ActionResult signup([FromBody] Users u)
         {
             Users us =  _context.Users.Find(u.Email);
@@ -147,7 +147,7 @@ namespace trial.Controllers
             }
             }
         [HttpPost("UploadFiles")]
-        [Route("/noteA")]
+        [Route("/noteArjit")]
         [Authorize]
         [Consumes("multipart/form-data")]
                 public ActionResult createNotes(NOTES n, IFormFile file){
